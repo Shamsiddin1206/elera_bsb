17bf35bd8b25f769fd088ef6f3714ee3ca0b9098
Shamsiddin1206 <takhirovshamsiddin@gmail.com>
2023-05-23 16:36:38 +0500

    1

--- app/src/main/java/com/example/elera/database/AppDatabase.kt
+++ app/src/main/java/com/example/elera/database/AppDatabase.kt
@@ -5,11 +5,14 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.elera.database.dao.User
+import com.example.elera.database.entity.CompletedLessons
+import com.example.elera.database.entity.Course
+import com.example.elera.database.entity.Lesson
 import com.example.elera.database.entity.Mentors
 import com.example.marks.entity.Student
 
 
-@Database(entities = [Student::class, Mentors::class], version = 1)
+@Database(entities = [Student::class, Mentors::class, Course::class, Lesson::class, CompletedLessons::class], version = 1)
 abstract class AppDatabase:RoomDatabase() {
     abstract fun getUserDao(): User
 
